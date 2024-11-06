8dfd0188bc4d85d106ca1c62b51bf52c511cb845
eawebmaster20 <eaweb20@gmail.com>
2024-11-06 19:56:36 +0100

    feat: bulk update

--- src/app/shared/services/api/api.service.ts
+++ src/app/shared/services/api/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { POD } from '../../models/picOfDayRes.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,15 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   picOfTheDay(){
-    return this.http.get(`${this.baseUrl}/planetary/apod?api_key=DEMO_KEY`);
+    return this.http.get<POD>(`${this.baseUrl}/planetary/apod`);
   }
   
-  marsRoverPhotos(date:string){
-    return this.http.get(`${this.baseUrl}/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=DEMO_KEY`);
+  marsRoverPhotos(){
+    return this.http.get(`${this.baseUrl}/mars-photos/api/v1/rovers/curiosity/photos?sol=1000`);
     // return this.http.get(`${this.baseUrl}/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3`);
   }
 
   getNeo(startDate:string, endDate:string){
-    return this.http.get(`${this.baseUrl}/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=DEMO_KEY`);
+    return this.http.get(`${this.baseUrl}/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}`);
   }
 }
