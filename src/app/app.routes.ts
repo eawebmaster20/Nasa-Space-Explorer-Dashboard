d750cba7d410804b4f1293c1705d6c4333d3c614
Emmanuel Adomako <emmanuel.adomako@bosonit.com>
2024-11-06 16:08:13 +0100

    feat: routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/auth', pathMatch: 'full' },
+    { path: 'auth', component: AuthComponent, children: [
+        { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) },
+    ]},
+    { path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+    // { path: 'favorites', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+    { path: '**', redirectTo: '/auth', pathMatch: 'full' }
+];
